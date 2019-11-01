3fb55efbb00e4b4e03aaf8d85b5b7d12622f8f8e
vlad <vladivashchuk@gmail.com>
2019-11-01 22:48:50 +0200

    light refactoring

--- client/computers/client.js
+++ client/computers/client.js
@@ -13,23 +13,3 @@ const Client = (baseUrl) => {
 
 module.exports = { Client };
 
-
-
-
-
-/*
-const Client = (baseUrl) => {
-
-    const client = http.Client(baseUrl);
-
-    return {
-        listComputers: () => client.get('/computers'),
-        createComputer: (name, cpuCount) => client.post('/computers', { name, cpuCount }),
-        createDisk: (idComputer, space) => client.post('/disks', { idComputer, space }),
-        listDisks: () => client.get ('/disks')
-    }
-
-};
-
-module.exports = { Client };
-*/
--- client/examples.js
+++ client/examples.js
@@ -19,7 +19,7 @@ client.listComputers()
     });
 
 
-
+// Scenario 2: Attach one of the discs to one of the computers
   client.setDisc()
 	.then((resp) => {
         console.log('=== Scenario 2 ===');
@@ -34,48 +34,3 @@ client.listComputers()
 
 
 
-
-
-/*
-// Scenario 1: Display available computers.
-client.listComputers()
-    .then((list) => {
-        console.log('=== Scenario 1 ===');
-        console.log('Available computers:');
-        Array.prototype.forEach.call(list, (c) => console.log(c.name));
-    })
-    .catch((e) => {
-        console.log(`Problem listing available channels: ${e.message}`);
-    });
-
-// Scenario 2: Create new channel.
-client.createComputer('my-new-channel')
-    .then((resp) => {
-        console.log('=== Scenario 2 ===');
-        console.log('Create computer response:', resp);
-        return client.listComputers()
-            .then((list) => Array.prototype.map.call(list, (c) => c.name).join(', '))
-            .then((str) => {
-                console.log(`Current channels: ${str}`);
-            })
-    })
-    .catch((e) => {
-        console.log(`Problem creating a new channel: ${e.message}`);
-    });
-
-client.listComputers()
-    .then((list) => Array.prototype.filter.call(list, (computer) => computer.id === 1))
-    .then((elem) => console.log(elem))
-    .catch((e) => {
-        console.log(`Problem listing available channels2: ${e.message}`);
-    });
-
-client.createDisk(1, 500)
-    .then((resp) => {
-        console.log('=== Scenario 3 ===');
-        console.log('Create disc response:', resp);
-        return client.listComputers()
-            .then((list) => Array.prototype.filter.call(list, (computer) => computer.id === 1))
-            .then((elem) => console.log(elem));
-    });
-	*/
